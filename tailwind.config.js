3b8ccebe868bb01aea2bdf2986414430edec0159
Juan Valdebenito <valdebenitovaldiviajuan@gmail.com>
2023-07-29 15:30:37 -0400

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        "background-color": "#0f0e17",
+        "button-color": "#ff8906",
+        "title-color": "#fffffe",
+        "white-primary": "#fffffe",
+        highlighted: "#e53170",
+        "color-black": "#000000",
+      },
+    },
+  },
+  plugins: ["tailwindcss", "autoprefixer"],
+};
